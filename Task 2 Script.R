57963b43a5a76ec2ddcc81730292e8df1f5ceab4
Rafotero00 <rd.otero@uniandes.edu.co>
2021-10-26 12:31:45 -0500

    Set Up

---
+++ Task 2 Script.R
@@ -0,0 +1,12 @@
+#Task 2 Taller R
+#Rafael Otero 201821640
+#Maria Paula Alvarez 201820569
+#Version de R: R version 4.1.0 (2021-05-18) ,Platform: x86_64-apple-darwin17.0 (64-bit), Running under: macOS Big Sur 11.5.2
+
+
+rm(list = ls())
+rm(list = ls()) # limpia el entorno de R
+if(!require(pacman)) install.packages(pacman)
+require(pacman)
+p_load(dplyr,data.table)
+print
